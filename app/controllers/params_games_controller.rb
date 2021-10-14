c1313bb2ac18093e406a2ec7bf50f1963b7a33f5
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-14 14:25:22 -0400

    Adds methods with wildcards

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -19,4 +19,26 @@ class ParamsGamesController < ApplicationController
 
     render json: { message: output_message }
   end
+
+  def number_add
+    input_value1 = params["variable_one"].to_i
+    input_value2 = params["variable_two"].to_i
+    addition = input_value1 + input_value2
+    render json: { message: "Your values together make #{addition}!" }
+  end
+
+  def guess_query
+    number = 36
+    input_value = params["wildcard"].to_i
+
+    if input_value == number
+      output_message = "Your number is the winning number!"
+    elsif number < input_value
+      output_message = "Your number is too high."
+    elsif number > input_value
+      output_message = "Your number is too low."
+    end
+
+    render json: { message: output_message }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/params_game" => "params_games#name_enter"
   get "/guess_number" => "params_games#number_guess"
+  get "/numbers:variable_one/:variable_2" => "params_games#numbers"
+  get "guess_query/:wildcard" => "params_games#guess_query"
 end
