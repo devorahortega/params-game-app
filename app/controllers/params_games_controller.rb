3dcceea5c89db9d7b2e4e2bc6e1b2db21cd8e272
Devorah Ortega <devorahb.ortega@gmail.com>
2021-10-14 12:57:39 -0400

    Creates a method to print user input (name) in all caps

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -1,2 +1,7 @@
 class ParamsGamesController < ApplicationController
+  def name_enter
+    input_value = params["my_name"]
+    input_value = input_value.upcase
+    render json: { message: "Your name is #{input_value}!" }
+  end
 end
